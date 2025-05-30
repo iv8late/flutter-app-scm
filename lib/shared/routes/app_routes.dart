d8079f68cf660832848240434e89bce3214896b9
ivo <ivadavila282003@gmail.com>
2025-05-29 23:21:05 -0500

    feat(login,signup,routes,forgot password)

--- lib/shared/routes/app_routes.dart
+++ lib/shared/routes/app_routes.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:project_base_app/auth/login.dart';
 import 'package:project_base_app/modules/404/pages/page_404.dart';
 
 class AppRoutes {
-  static const initialRoute = '/splash';
+  static const initialRoute = '/login';
 
   static Map<String, Widget Function(BuildContext)> routes = {
-    
+    '/login': (context) => const LoginScreen(),
     // '/settings': (context) => const SettingsScreen(),
   };
 
   static Route<dynamic> onGenerateRoute(RouteSettings settings) {
-    return MaterialPageRoute(
-      builder: (context) => const PageNotFound(),
-    );
+    return MaterialPageRoute(builder: (context) => const PageNotFound());
   }
 }
